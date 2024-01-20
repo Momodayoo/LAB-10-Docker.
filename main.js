0fc6e9585f7ee93f74c72d7c4ffae28f567fbddb
Momodayoo <msyauqi94@gmail.com>
2024-01-20 09:36:32 +0800

    commit

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use (express.static(__dirname +));
+
+app.listen(proces.env.PORT || 8080);
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 8080,
-    host: "0.0.0.0"
-  }
-  })
+    })
